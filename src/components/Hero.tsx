7046dc5073b89a270f7b6910c27ec067901455e1
Isaac Farciert <rfisaac2296@yahoo.com>
2022-10-10 09:20:09 -0400

    add hero base

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import SingleImageCarousel from "./utils/SingleImageCarousel";
 
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="p-2">
+      <h3 className="text-xl font-bold">
+        We make building a new home possible
+      </h3>
+      <p className="p-4 bg-amber-100">
+        A new home is so much better than a used one. Welcome makes the process
+        of building a home super-simple. We handle everything at & guaranteed,
+        all-in-one price.
+      </p>
+
+      <button className="border px-6 bg-sky-600 rounded">Our models</button>
+      <button className="border px-4">Explore towns</button>
+      <SingleImageCarousel />
+    </section>
+  );
 }
