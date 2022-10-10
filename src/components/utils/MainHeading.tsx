95b4149199b99caaa8f7b8b7e2b07665f395bf25
Isaac Farciert <rfisaac2296@yahoo.com>
2022-10-10 19:05:55 -0400

    add support for overriding css classes

--- src/components/utils/MainHeading.tsx
+++ src/components/utils/MainHeading.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 type Props = {
   children: React.ReactNode;
+  className?: string;
 };
 
-export default function MainHeading({ children }: Props) {
-  return <h2 className="font-bold text-xl">{children}</h2>;
+export default function MainHeading({ children, className }: Props) {
+  return <h2 className={`font-bold text-xl ${className}`}>{children}</h2>;
 }
