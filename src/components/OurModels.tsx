300079679cc63eb742d909294b6b6831a5c2fb30
Isaac Farciert <rfisaac2296@yahoo.com>
2022-10-13 15:41:18 -0400

    add our models skeleton

--- src/components/OurModels.tsx
+++ src/components/OurModels.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import BoldHeadline from "./utils/BoldHeadline";
+import MainSection from "./utils/MainSection";
+import SecSubheading from "./utils/SecSubheading";
 
 export default function OurModels() {
-  return <div>OurModels</div>;
+  return (
+    <MainSection>
+      <BoldHeadline className="text-center">Our Models</BoldHeadline>
+      <SecSubheading className="text-center">
+        Compare our models to find the perfect home for you, customized to your
+        liking.
+      </SecSubheading>
+      <div className="bg-yellow-100">Tabbed model viewer</div>
+    </MainSection>
+  );
 }
